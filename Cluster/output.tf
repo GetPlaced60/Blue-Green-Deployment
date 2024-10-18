fb526d3e957d474ce960b8365bd4fc2156c5698e
GetPlaced60 <getplaced60@gmail.com>
2024-10-18 12:37:15 +0530

    updated

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.getplaced.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.getplaced.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.getplaced_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.getplaced_subnet[*].id
 }
 
